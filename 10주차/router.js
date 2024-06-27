f38a3e6bfa360aa2f43c11623352418b25f28400
unknown <ats0830@naver,com>
2024-06-27 20:30:05 +0900

    temp

---
+++ 10주차/router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+app.get('/list', (req,res)=>{
+    
+})
+app.get('/write', (req,res)=>{
+    
+})
+app.get('/view', (req,res)=>{
+    
+})
+app.get('/update', (req,res)=>{
+    
+})
+app.get('/update', (req,res)=>{
+    
+})
+app.get('/delete', (req,res)=>{
+    
+})
+
+module.exports = router;
+
